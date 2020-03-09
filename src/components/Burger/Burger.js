1f894e8ac9f5b3712b6b222770f223f771c9d24e
Pedro Pradp <cin_pprado@uolinc.com>
2020-03-09 19:48:05 -0300

    Adding more ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,16 +4,25 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+
+    const transformedIngredients = Object.keys(props.ingredients)
+    .map( ingredient => {
+        return [...Array(props.ingredients[ingredient])].map( (_, i) => {
+           return <BurgerIngredient key={ingredient + i}  type={ingredient}/>
+        });
+    });
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            { transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
 };
 
+burger.propTypes ={
 
+}
 
 export default burger;
